3823e9c490f5c38d2297ad5512e7cca11ff41c74
BernardoDuran14 <bernardo.duran.mamani@gmail.com>
2025-04-29 14:16:32 -0400

    Agregando funciones voltear tarjeta y modificando diseño en ambas caras

--- scripts.js
+++ scripts.js
@@ -3,4 +3,10 @@ const verDetallesButton = document.getElementById("verDetalles");
 const volverButton = document.getElementById("volver");
 const tarjeta = document.querySelector(".tarjeta");
 // Función para voltear la tarjeta
+verDetallesButton.addEventListener("click", () => {
+    tarjeta.classList.add("volteada");
+  });
 // Función para volver a la cara frontal
+volverButton.addEventListener("click", () => {
+    tarjeta.classList.remove("volteada");
+  });
